f8668a7d1ab70f1c8219d8f15cd0cebda69424f2
Sagara <sagara.gunathunga@gmail.com>
2014-08-22 14:04:20 +0530

    add code

---
+++ src/main/java/org/wso2/carbon/transport/jms/clusteraware/ClusterAwareJMSListener.java
@@ -0,0 +1,114 @@
+/*
+*  Copyright (c) 2005-2011, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.carbon.transport.jms.clusteraware;
+
+import com.hazelcast.core.MembershipEvent;
+import com.hazelcast.core.MembershipListener;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.clustering.ClusteringAgent;
+import org.apache.axis2.transport.jms.JMSEndpoint;
+import org.apache.axis2.transport.jms.JMSListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.base.MultitenantConstants;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+public class ClusterAwareJMSListener extends JMSListener implements MembershipListener {
+
+    private static final Log log = LogFactory.getLog(ClusterAwareJMSListener.class);
+
+    private List<JMSEndpoint> endpointList = new ArrayList<JMSEndpoint>();
+    private static final int DEFAULT_COORDINATOR_ELECTION_DELAY = 60000;
+    private static final String COORDINATOR_ELECTION_DELAY_PROPERTY_NAME = "transport.jms.coordinatorElectionDelay";
+    private boolean currentCoordinator = false;
+    private int coordinatorElectionDelay = -1;
+
+
+    @Override
+    protected void startEndpoint(JMSEndpoint endpoint) throws AxisFault {
+        setCoordinatorElectionDelay(endpoint);
+        if (currentCoordinator) {
+            if (log.isDebugEnabled()) {
+                log.debug(endpoint.getServiceName() + "start now ! ");
+            }
+            super.startEndpoint(endpoint);
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug(endpoint.getServiceName() + " will add to endpointList, now start now ");
+            }
+            endpointList.add(endpoint);
+        }
+    }
+
+    public void startEndpoints() {
+        log.debug(" Start adding endpoints in endpointList ");
+        for (JMSEndpoint ep : endpointList) {
+            try {
+                super.startEndpoint(ep);
+                log.info(ep.getServiceName() + " started ! ");
+            } catch (AxisFault axisFault) {
+                axisFault.printStackTrace();
+            }
+        }
+        endpointList.clear();
+    }
+
+
+    public void memberAdded(MembershipEvent membershipEvent) {
+
+    }
+
+    public void memberRemoved(MembershipEvent membershipEvent) {
+        PrivilegedCarbonContext.startTenantFlow();
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(MultitenantConstants.SUPER_TENANT_ID);
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(MultitenantConstants.SUPER_TENANT_DOMAIN_NAME);
+        // Set delay due to it take some times to zookeeper to sync data such as global queue when member left cluster
+        try {
+            Thread.sleep(coordinatorElectionDelay);
+        } catch (InterruptedException ignore) {
+
+        }
+        ClusteringAgent agent = ClusterAwareJMSDataHolder.getInstance().getClusteringAgent();
+        if (agent.isCoordinator()) {
+            startEndpoints();
+            setCurrentCoordinator(true);
+        }
+    }
+
+    public void setCurrentCoordinator(boolean currentCoordinator) {
+        this.currentCoordinator = currentCoordinator;
+    }
+
+    public void setCoordinatorElectionDelay(JMSEndpoint ep) {
+        if (-1 == coordinatorElectionDelay) {
+            Hashtable<String, String> properties = ep.getServiceTaskManager().getJmsProperties();
+            String delayStr = properties.get(COORDINATOR_ELECTION_DELAY_PROPERTY_NAME);
+            if (delayStr != null && !"".equals(delayStr)) {
+                this.coordinatorElectionDelay = Integer.valueOf(delayStr.trim());
+            } else {
+                this.coordinatorElectionDelay = DEFAULT_COORDINATOR_ELECTION_DELAY;
+            }
+            log.info("coordinatorElectionDelay value set to " + coordinatorElectionDelay);
+        }
+    }
+}
---
+++ src/main/java/org/wso2/carbon/transport/jms/clusteraware/ClusterAwareJMSListenerComponent.java
@@ -0,0 +1,106 @@
+package org.wso2.carbon.transport.jms.clusteraware;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.MembershipListener;
+import org.apache.axis2.Constants;
+import org.apache.axis2.clustering.ClusteringAgent;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.description.TransportInDescription;
+import org.apache.axis2.engine.ListenerManager;
+import org.apache.axis2.transport.TransportListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.utils.ConfigurationContextService;
+
+/**
+ * @scr.component  name="org.wso2.carbon.transport.jms.clusteraware.ClusterAwareJMSListenerComponent"
+ *                              immediate="true"
+ * @scr.reference  name="hazelcast.instance.service"
+ *                              interface="com.hazelcast.core.HazelcastInstance"
+ *                              cardinality="1..1"
+ *                              policy="dynamic"
+ *                              bind="setHazelcastInstance"
+ *                              unbind="unsetHazelcastInstance"
+ * @scr.reference  name="config.context.service"
+ *                              interface="org.wso2.carbon.utils.ConfigurationContextService"
+ *                              cardinality="1..1" policy="dynamic"
+ *                              bind="setConfigurationContextService"
+ *                              unbind="unsetConfigurationContextService"
+ * @scr.reference  name="ListenerManager.component"
+ *                              interface="org.apache.axis2.engine.ListenerManager"
+ *                              cardinality="1..1"
+ *                              policy="dynamic"
+ *                              bind="setListenerManager"
+ *                              unbind="unSetListenerManager"
+ */
+public class ClusterAwareJMSListenerComponent {
+
+    private static final Log log = LogFactory.getLog(ClusterAwareJMSListenerComponent.class);
+
+    private HazelcastInstance hazelcastInstance;
+    private ConfigurationContext configurationContext;
+    private ClusteringAgent agent;
+
+    protected void activate(ComponentContext ctx) {
+
+        log.debug("in activate method");
+        if (configurationContext != null) {
+            TransportInDescription jmsIn = configurationContext.getAxisConfiguration().getTransportIn(Constants.TRANSPORT_JMS);
+            if (jmsIn != null) {
+                TransportListener listener = jmsIn.getReceiver();
+                if (listener != null && listener instanceof ClusterAwareJMSListener) {
+                    if (isCurrentCoordinator()) {
+                        log.debug("This member is the current coordinator");
+                        ClusterAwareJMSListener clusterAwareJMSListener = (ClusterAwareJMSListener) listener;
+                        clusterAwareJMSListener.setCurrentCoordinator(true);
+                        clusterAwareJMSListener.startEndpoints();
+                    } else {
+                        log.debug("This member is not current coordinator");
+                        hazelcastInstance.getCluster().addMembershipListener((MembershipListener) listener);
+                    }
+                }
+            }
+        }
+    }
+
+
+    protected void deactivate(ComponentContext ctx) {
+    }
+
+//    public boolean isCurrentCoordinator() {
+//        ClusteringAgent agent = ClusterAwareJMSDataHolder.getInstance().getClusteringAgent();
+//        if (agent != null) {
+//            return agent.isCoordinator();
+//        }
+//        return false;
+//    }
+
+    public boolean isCurrentCoordinator() {
+        return agent.isCoordinator();
+    }
+
+    protected void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+    protected void unsetHazelcastInstance(HazelcastInstance hazelcastInstance) {
+    }
+
+    protected void setConfigurationContextService(ConfigurationContextService configurationContextService) {
+        this.configurationContext = configurationContextService.getServerConfigContext();
+        agent = configurationContextService.getServerConfigContext().getAxisConfiguration().getClusteringAgent();
+        ClusterAwareJMSDataHolder.getInstance().setClusteringAgent(agent);
+    }
+
+    protected void unsetConfigurationContextService(ConfigurationContextService configurationContextService) {
+        ClusterAwareJMSDataHolder.getInstance().setClusteringAgent(null);
+
+    }
+
+    protected void setListenerManager(ListenerManager listenerManager) {
+    }
+
+    protected void unSetListenerManager(ListenerManager listenerManager) {
+    }
+}
